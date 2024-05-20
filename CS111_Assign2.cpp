383249ef4b6ae81675b4e7f43e0f8c9bcc432c39
Vishant Kumar <163496021+v-i-s-h-a-n-t@users.noreply.github.com>
2024-05-20 23:05:40 +1200

    added void functions

--- CS111_Assign2.cpp
+++ CS111_Assign2.cpp
@@ -1,20 +1,23 @@
 #include <iostream>
+#include <iomanip>
 #include <fstream>
 #include <string>
 
 using namespace std;
 
-int input_validation()
+int validate_input()
 {
 	const int LOWER_LIMIT = 1;
 	const int UPPER_LIMIT = 6;
     int input;
     cin >> input;
-    while(input < LOWER_LIMIT  || input < UPPER_LIMIT || cin.fail()){
+    
+    while(input < LOWER_LIMIT  || input > UPPER_LIMIT || cin.fail())
+	{
         cin.clear();
-        cout << "Incorrect input. Please enter numbers 1-6 only" << endl;
         string dummy;
         cin >> dummy;
+        cout << "ERROR!. Please enter a number between 1-6 only" << endl;
         cin >> input;
     }
     return input;
@@ -36,19 +39,63 @@ void exit_program()
     cout << "+----------------------------------+\n";
 }
 
+void program_start_quit(bool& continue_running)
+{
+	char user_decision;
+	cout << "\n Enter S to start or Q to quit";
+	cin >> user_decision;
+	
+	while(user_decision != 'S' && user_decision != 's' && user_decision != 'Q' && user_decision != 'q')
+	{
+		cin.clear();
+		string dummy;
+		cin >> dummy;
+		cout << "ERROR!, invalid input was detected" << endl;
+		cout << "Please enter either \"S\" or \"Q\" only" << endl;
+		cin >> user_decision;
+	}
+	
+	if (user_decision == 'S' || user_decision == 's')
+	{
+		continue_running = true;
+	}
+	
+	else if (user_decision == 'Q' || user_decision == 'q')
+	{
+		continue_running = false;
+	}
+}
+
+void fill_array(string file_name, int& rows, int student_id[], string student_name[], double maths_marks[], double science_marks[], double english_marks)
+{
+	string remove_header;
+	
+	ifstream read_file;
+	read_file.open(file_name);
+	
+	if(!read_file)
+	{
+		cout << "ERROR!. This file could not be found" << endl;
+	}
+	
+	else
+	{
+		getline(read_file, remove_header);
+		
+		while(read_file >> student_id[rows] >> student_name[rows] >> maths_marks[rows] >> science_marks[rows] >> english_marks[rows])
+		{
+			rows++;
+		}
+	}
+}
+
 int main()
 {
-    welcome_message();
-    ifstream output;
-    output.open("studentmarks.txt");
-    output.close();
+	int choice;
+	int rows = 0;
     
-    ofstream input;
-    input.open("summary.txt");
-    string index;
-    string student_name;
-
-    int choice;
+    //Constant declarations
+    const int CAPACITY = 300;
     const int PRINT_LIST = 1;
     const int PRINT_DETAILS = 2;
     const int PRINT_TOTAL = 3;
@@ -56,28 +103,17 @@ int main()
     const int WRITE_REPORT = 5;
     const int EXIT_PROGRAM = 6;
     
-    cout << "Menu" << endl 
-         << "1) Print the Entire List" << endl 
-         << "2) Print Details of Student Matching a Given ID" << endl
-         << "3) Calculate Total and Print Entire List with Total" << endl
-         << "4) Print List sorted by Total" << endl
-         << "5) Write Report to File" << endl
-         << "6) Exit Program" <<  endl
-         << "Enter your choice"; 
-    choice = input_validation();
+    bool continue_running = true;
     
-    switch(choice){
-            case 1: cout << index << student_name;
-            case 2: cout << index << student_name;
-            case 3: cout << index << student_name;
-            case 4: cout << index << student_name;
-            case 5: cout << index << student_name;
-            case 6: cout << index << student_name;
-        }
-
-    input.close();
-
-    exit_program();
+    //Arrays for storing different categories of data
+    int student_id[CAPACITY];
+    string student_name[CAPACITY];
+    double maths_marks[CAPACITY];
+    double science_marks[CAPACITY];
+    double english_marks[CAPACITY];
+    
+    welcome_message();
+	
 
     return 0;
 }
