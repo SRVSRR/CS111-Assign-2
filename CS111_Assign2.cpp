9312ef35762960223c4701ef4c71f303a4f7947b
Vishant Kumar <163496021+v-i-s-h-a-n-t@users.noreply.github.com>
2024-05-20 15:56:05 +1200

    added message functions

--- CS111_Assign2.cpp
+++ CS111_Assign2.cpp
@@ -5,9 +5,28 @@
 
 using namespace std;
 
+void welcome_message()
+{
+	cout << "+----------------------------------------------------+" << endl;
+	cout << "|  WELCOME TO THE STUDENT MARKS MANAGEMENT PROGRAM!  |" << endl;
+	cout << "+----------------------------------------------------+" << endl;
+	
+}
+
+void exit_message()
+{
+	cout << "+------------------------------------+" << endl;
+	cout << "|  THANK YOU FOR USING THIS PROGRAM  |" << endl;
+	cout << "+------------------------------------+" << endl;
+}
+
 int main()
 {
-
+	welcome_message();
+	
+	cout << endl;
+	
+	exit_message();
 
 	return 0;
 }
